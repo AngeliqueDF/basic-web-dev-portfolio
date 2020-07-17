40be6438baf92e84f3a9d20e98b6ef18874ccd7e
Angélique Faye <faye.angelique.d@gmail.com>
2020-07-17 13:08:01 +0200

    css, single template

--- front-page.php
+++ front-page.php
@@ -7,73 +7,74 @@
     </section>
 
     <div class="container">
-        <section class="projects">
-        <h2>Réalisations</h2>
-        <div class="projects-list">
-            <?php
-            $args = array(
-                // 'posts_per_page' => 3,
-                'order' => 'DESC',
-                'category_name' => 'realisations'
-            );
-            // The Query
-            $projects_query = new WP_Query($args);
+        <section class="skills">
+            <h2>Compétences</h2>
+            <h3>Certifications</h3>
+            <ul>
+                <li><a href="https://directory.opquast.com/fr/certificat/P0MO5X/">Opquast, la certification des professionnels du Web.</a></li>
+            </ul>
+            <h3>Langages</h3>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript (ES6)</li>
+                <li>MySQL CRUD</li>
+                <li>PHP OOP, MVC</li>
+            </ul>
 
-            if ($projects_query->have_posts()) {
-                while ($projects_query->have_posts()) {
-                    $projects_query->the_post();
-                    ?>
-                    <!-- include template parts according to post format -->
-                    <article id="post-<?php the_ID(); ?>" <?php post_class('project'); ?>>
-                        <header>
-                            <div class="project-type"><?php the_category(); ?></div>
+            <h3>Outils</h3>
+            <ul>
+                <li>Gulp</li>
+                <li>Sass</li>
+                <li>Git</li>
+            </ul>
 
-                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                            <?php get_template_part('links-to-code-demos');?>
-                        </header>
-
-                        <footer>
-                            <div class="post-tags">
-                                <?php if (has_tag()) { ?>
-                                <?php the_tags('<ul><li>', '</li><li>', '</li></ul>');
-                                        } ?>
-                            </div>
-                            <p class="article-author"><small><?php the_author_posts_link(); ?></small></p>
-                        </footer>
-                    </article>
-            <?php } // end while
-            } // end if
-            else {
-                echo "no post";
-            }
-            ?>
-
+            <p><a href="https://github.com/angelique-df/">Github</a></p>
         </section>
     </div>
 
     <div class="container">
-        <section class="skills">
-            <h2>Compétences</h2>
-                <h3>Certifications</h3>
-                <ul>
-                    <li><a href="https://directory.opquast.com/fr/certificat/P0MO5X/">Opquast, la certification des professionnels du Web.</a></li>
-                </ul>
-                <h3>Langages</h3>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>MySQL</li>
-                    <li>PHP</li>
-                </ul>
+        <section class="projects">
+            <h2>Réalisations</h2>
+            <div class="projects-list">
+                <?php
+                $args = array(
+                    // 'posts_per_page' => 3,
+                    'order' => 'DESC',
+                    'category_name' => 'realisations'
+                );
+                // The Query
+                $projects_query = new WP_Query($args);
 
-                <h3>Outils</h3>
-                <ul>
-                    <li>Sass</li>
-                    <li>Git</li>
-                </ul>
+                if ($projects_query->have_posts()) {
+                    while ($projects_query->have_posts()) {
+                        $projects_query->the_post();
+                ?>
+                        <!-- include template parts according to post format -->
+                        <article id="post-<?php the_ID(); ?>" <?php post_class('project'); ?>>
+                            <header>
+                                <div class="project-type"><?php the_category(); ?></div>
 
-            <p><a href="https://github.com/angelique-df/">Github</a></p>
+                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <?php get_template_part('links-to-code-demos'); ?>
+                            </header>
+
+                            <footer>
+                                <div class="post-tags">
+                                    <?php if (has_tag()) { ?>
+                                    <?php the_tags('<ul><li>', '</li><li>', '</li></ul>');
+                                    } ?>
+                                </div>
+                                <p class="article-author"><small><?php the_author_posts_link(); ?></small></p>
+                            </footer>
+                        </article>
+                <?php } // end while
+                } // end if
+                else {
+                    echo "no post";
+                }
+                ?>
+
         </section>
     </div>
 
--- single.php
+++ single.php
@@ -1,59 +1,61 @@
 <?php get_header(); ?>
 
 
-    <main>
-        <div class="container">
-        <?php 
+<main>
+    <div class="container">
+        <?php
         the_breadcrumb();
         if (have_posts()) {
             while (have_posts()) {
                 the_post();
-                ?>
-                    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <article class="post-content">
-                            <header>
+        ?>
+                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <article class="post-content">
+                        <header>
 
-                                <h1><?php the_title(); ?></h1>
+                            <img src="<?= get_the_post_thumbnail_url() ?>" alt="">
 
-                                <div class="meta">
-                                    <?php the_category(); ?>
-                                    <p><?php the_date(); ?></p>
-                                    <p><?php the_author(); ?></p>
-                                    
-                                    <?php get_template_part("links-to-code-demos"); ?> 
-                                </div>
-                            </header>
-                            <?php wp_link_pages(); ?>
+                            <h1><?php the_title(); ?></h1>
+                            <div class="meta">
+                                <?php the_category(); ?>
+                                <p><?php the_date(); ?></p>
+                                <p><?php the_author(); ?></p>
 
-                            <div class="main-article-content">
-                                <?php the_content(); ?>
+                                <?php get_template_part("links-to-code-demos"); ?>
                             </div>
-                                
-                            <?php wp_link_pages(); ?>
+                        </header>
+                        <?php wp_link_pages(); ?>
 
-                            <footer>
-                            <!-- <p><?php //the_author_posts_link(); ?></p> -->
-                                <div class="post-tags">
-                                    <?php the_tags('<ul><li>', '</li><li>', '</li></ul>'); ?>
-                                </div>
-                                
-                            </footer>
-                        </article>
-                    </div>
-                    <?php } // end while
+                        <div class="main-article-content">
+                            <?php the_content(); ?>
+                        </div>
+
+                        <?php wp_link_pages(); ?>
+
+                        <footer>
+                            <!-- <p><?php //the_author_posts_link(); 
+                                    ?></p> -->
+                            <div class="post-tags">
+                                <?php the_tags('<ul><li>', '</li><li>', '</li></ul>'); ?>
+                            </div>
+
+                        </footer>
+                    </article>
+                </div>
+        <?php } // end while
         } // end if
         ?>
-        </div>
+    </div>
 
-        <div class="container">
-            <?php 
-                // If comments are open or we have at least one comment, load up the comment template.
-                if (comments_open() || get_comments_number()) :
-                    comments_template();
-                endif;
-            ?>
-        </div>
-    </main>
+    <div class="container">
+        <?php
+        // If comments are open or we have at least one comment, load up the comment template.
+        if (comments_open() || get_comments_number()) :
+            comments_template();
+        endif;
+        ?>
+    </div>
+</main>
 </div><!-- /.container -->
 <?php
 get_footer();
